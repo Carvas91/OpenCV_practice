ecfd2c77f907a8ddf8d52bf7be54e4cb67934e1f
carvas91 <carvas91@hotmail.com>
2024-11-04 15:53:18 -0500

    commit

--- drawings/drawings.py
+++ drawings/drawings.py
@@ -11,7 +11,9 @@ image[:] = 255,0,0
 
 line = cv2.line(image, (0,0), (image.shape[1],image.shape[0]),(0,0,255),3 )
 
-cv2.imshow('Output', line)
+text = cv2.putText(line, "OpenCV Practice", (300,100), cv2.FONT_HERSHEY_COMPLEX, 1, (100,100,200), 5)
+
+cv2.imshow('Output', text)
 
 
 cv2.waitKey(0)
